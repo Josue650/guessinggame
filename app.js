4e0ab32c60493261d5ebed536d21ede8ec804bb5
Josue <cuellarjosue3@gmail.com>
2022-09-13 20:17:08 -0500

    Game works, Added a level 2 and 3

--- app.js
+++ app.js
@@ -1,25 +1,34 @@
 const submit = document.getElementById('submit-num-guess')
+const guessed = document.querySelector('.guessed');
 
-
+const arr = [10, 20, 30]
 //Generates a random value
-const randomVal = Math.floor(Math.random() * 10 + 1)
+let i = 0;
+let randomVal = Math.floor(Math.random() * arr[i])
 
 
 //Number of guesses
 let guess = 1;
 
+//Click event listener to check the number
 submit.addEventListener('click', (evt)=>{
 
     let guessedVal = document.getElementById('guess-input').value;
 
-    if(guessedVal == randomVal){
-        alert("You guessed the numner right! Only took " + guess + " guesses" )
-    } else if (guessedVal > randomVal) {
-        guess++;
-        alert("No no no try again number is too big")
+    if(guessedVal == randomVal){ // Check if the number entered is equal to the target value
+        guessed.innerHTML = "You guessed the numner right! Only took " + guess + " guesses" + "<button id='round'>Next Round</button>" // Adds message to the screen
+        document.getElementById('round').addEventListener('click', (evt) => {
+            i++
+            document.getElementById('max-value').innerHTML = arr[i]
+            guessed.innerHTML = ''
+        })
+    } else if (guessedVal > randomVal) { // Check if the number is greater than target value
+        guess++; // Increment count after every guess
+        guessed.innerHTML = "No no no try again number is too big"
     } else {
-        guess++;
-        alert("No no no try again number is too small")
+        guess++; // Increment count after every guess
+        guessed.innerHTML = "No no no try again number is too small"
     }
+
 
 })
