f51b62fa30bacd612d5b246c067df6a0c5afac3f
Josue <cuellarjosue3@gmail.com>
2022-09-17 10:11:26 -0500

    New Game name

--- app.js
+++ app.js
@@ -1,13 +1,15 @@
 const submit = document.getElementById('submit-num-guess')
 const guessed = document.querySelector('.guessed');
+const scoreCount = document.querySelector('.score')
 
 const arr = [10, 20, 30]
 //Generates a random value
 let i = 0;
 let randomVal = Math.floor(Math.random() * arr[i])
 
-
-//Number of guesses
+// Score counter
+let score = 0;
+// Number of guesses
 let guess = 1;
 
 //Click event listener to check the number
@@ -15,12 +17,13 @@ submit.addEventListener('click', (evt)=>{
 
     let guessedVal = document.getElementById('guess-input').value;
 
-    if(guessedVal == randomVal){ // Check if the number entered is equal to the target value
+    if(guessedVal == randomVal ){ // Check if the number entered is equal to the target value
         guessed.innerHTML = "You guessed the numner right! Only took " + guess + " guesses" + "<button id='round'>Next Round</button>" // Adds message to the screen
         document.getElementById('round').addEventListener('click', (evt) => {
             i++
             document.getElementById('max-value').innerHTML = arr[i]
             guessed.innerHTML = ''
+            score.innerHTML = "You're score is" + score 
         })
     } else if (guessedVal > randomVal) { // Check if the number is greater than target value
         guess++; // Increment count after every guess
