60e6b52ed31aae256e9d8a39e3254898f701281d
Josue <cuellarjosue3@gmail.com>
2022-09-17 15:14:17 -0500

    If you win game reloads

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const submit = document.getElementById('submit-num-guess')
 const guessed = document.querySelector('.guessed');
 const scoreCount = document.querySelector('.score')
-
+// const tooMany = document.querySelector('.too-many')
 const arr = [10, 20, 30]
 //Generates a random value
 let i = 0;
@@ -15,22 +15,37 @@ let guess = 1;
 //Click event listener to check the number
 submit.addEventListener('click', (evt)=>{
 
+
     let guessedVal = document.getElementById('guess-input').value;
 
-    if(guessedVal == randomVal ){ // Check if the number entered is equal to the target value
-        guessed.innerHTML = "You guessed the numner right! Only took " + guess + " guesses" + "<button id='round'>Next Round</button>" // Adds message to the screen
+    if(guessedVal == randomVal){ // Check if the number entered is equal to the target value
+        guessed.innerHTML = "You guessed the number right! Only took " + guess + " guesses" + "<button id='round'>Next Round</button>" // Adds message to the screen
         document.getElementById('round').addEventListener('click', (evt) => {
             i++
             document.getElementById('max-value').innerHTML = arr[i]
             guessed.innerHTML = ''
-            score.innerHTML = "You're score is" + score 
+            score++
+            scoreCount.innerHTML = "You're score is " + score
+            randomVal = Math.floor(Math.random() * arr[i])
+
+  
+
+    }
         })
     } else if (guessedVal > randomVal) { // Check if the number is greater than target value
         guess++; // Increment count after every guess
         guessed.innerHTML = "No no no try again number is too big"
+        if(guess > 3){
+            alert('You took too many guesses. You have lost!')
+            document.location.reload()
+        }
     } else {
         guess++; // Increment count after every guess
         guessed.innerHTML = "No no no try again number is too small"
+        if(guess > 3){
+            alert('You took too many guesses. You have lost!')
+            document.location.reload()
+        }
     }
 
 
